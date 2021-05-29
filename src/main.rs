e58abcf791830297d8fcb6a5aa4f1eba120148b4
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-05-29 02:17:22 +0200

    fly an arrow around

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use ggez::{graphics, Context, ContextBuilder, GameResult};
-use ggez::conf::{WindowSetup, NumSamples};
+use ggez::conf::{NumSamples, WindowSetup};
 use ggez::event::{self, EventHandler};
+use ggez::nalgebra::{self as na, Point2, Vector2};
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::input::keyboard::{self, KeyCode};
 
 fn main() {
     let window_setup = WindowSetup {
@@ -11,46 +13,89 @@ fn main() {
         srgb: true,
     };
 
-    // Make a Context.
     let (mut ctx, mut event_loop) = ContextBuilder::new("timmy_hunting", "Emil Wasilewski")
         .window_setup(window_setup)
-		.build()
-		.expect("aieee, could not create ggez context!");
+        .build()
+        .expect("aieee, could not create ggez context!");
 
-    // Create an instance of your event handler.
-    // Usually, you should provide it with the Context object to
-    // use when setting your game up.
     let mut my_game = MyGame::new(&mut ctx);
 
-    // Run!
     match event::run(&mut ctx, &mut event_loop, &mut my_game) {
         Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occurred: {}", e)
+        Err(e) => println!("Error occurred: {}", e),
     }
 }
 
 struct MyGame {
-    // Your state here...
+    pub rotation: f32,
+    pub direction: na::Rotation2<f32>, 
+    pub position: Point2<f32>,
+    pub triangle_mesh: graphics::Mesh,
 }
 
 impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
-        // Load/create resources such as images here.
+    pub fn new(ctx: &mut Context) -> MyGame {
         MyGame {
-		    // ...
-		}
+            rotation: 0.0,
+            direction: na::Rotation2::new(0.0),
+            position: Point2::new(300.0, 400.0),
+            triangle_mesh: graphics::MeshBuilder::new()
+                .line(
+                    &[
+                        Point2::new(15.0, 15.0),
+                        Point2::new(0.0, -15.0),
+                        Point2::new(-15.0, 15.0),
+                    ],
+                    2.0,
+                    graphics::WHITE,
+                )
+                .unwrap()
+                .build(ctx)
+                .unwrap(),
+        }
     }
 }
 
+fn move_player(position: &mut Point2<f32>, direction: &na::Rotation2<f32>, dt: f32, ctx: &Context) -> GameResult<()>{
+    let mut movement: f32 = 0.0;
+    if keyboard::is_key_pressed(ctx, KeyCode::W){
+        movement = -100.0;
+    }else if keyboard::is_key_pressed(ctx, KeyCode::S){
+        movement = 100.0;
+    }
+    let delta = direction * Vector2::new(0.0, movement) * dt;
+    position.y += delta.y;
+    position.x += delta.x;
+
+    Ok(())
+}
+
 impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
-		Ok(())
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let dt = ggez::timer::delta(ctx).as_secs_f32();
+        move_player(&mut self.position, &self.direction, dt, &ctx)?;
+
+        let mut r = na::Rotation2::new(0.0);
+        if keyboard::is_key_pressed(ctx, KeyCode::D){
+            r = na::Rotation2::new(1.0 * dt);
+        }else if keyboard::is_key_pressed(ctx, KeyCode::A){
+            r = na::Rotation2::new(-1.0 * dt);
+        }
+        self.direction = r * self.direction;
+
+        self.rotation = self.direction.angle();
+        Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-		graphics::clear(ctx, graphics::BLACK);
-        // Draw code here...
-		graphics::present(ctx)
+        graphics::clear(ctx, graphics::BLACK);
+        
+        graphics::draw(
+            ctx,
+            &self.triangle_mesh,
+            (self.position, self.rotation, graphics::WHITE),
+        )?;
+
+        graphics::present(ctx)
     }
 }
