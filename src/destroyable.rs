96d400d2f260f80c475f109c816884b879237b36
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-06-14 00:04:37 +0200

    change rendering for collidable

--- src/destroyable.rs
+++ src/destroyable.rs
@@ -1,14 +1,34 @@
-use ggez::nalgebra::{Point2, Vector2};
 use ggez::{graphics, Context, GameResult};
+use nalgebra::{Isometry2, Point2, Vector2};
+use ncollide2d::shape::Polyline;
+
+use crate::collision_system::Collidable;
+use crate::render_system::line_mesh::LineMeshRenderer;
+use crate::render_system::Renderable;
 
 use crate::physic;
 
 const _RED: graphics::Color = graphics::Color::new(1.0, 0.0, 0.0, 1.0);
 
+fn create_cube_mesh(ctx: &mut Context) -> ggez::graphics::Mesh {
+    let mesh_points: [ggez::nalgebra::Point2<f32>; 5] = [
+        ggez::nalgebra::Point2::new(15.0, 15.0),
+        ggez::nalgebra::Point2::new(15.0, -15.0),
+        ggez::nalgebra::Point2::new(-15.0, -15.0),
+        ggez::nalgebra::Point2::new(-15.0, 15.0),
+        ggez::nalgebra::Point2::new(15.0, 15.0),
+    ];
+    ggez::graphics::MeshBuilder::new()
+        .line(&mesh_points, 2.0, ggez::graphics::WHITE)
+        .unwrap()
+        .build(ctx)
+        .expect("Could not build PLayer Mesh")
+}
+
 pub struct Destroyable {
     pub transform: physic::Transform,
     pub body: physic::Body,
-    pub mesh: graphics::Mesh,
+    pub renderer: LineMeshRenderer,
     pub render_color: graphics::Color,
 }
 
@@ -20,43 +40,29 @@ impl Destroyable {
                 rotation: 0.0,
             },
             body: physic::Body::new(position, 0.99),
-            mesh: graphics::MeshBuilder::new()
-                .line(
-                    &[
-                        Point2::new(15.0, 15.0),
-                        Point2::new(15.0, -15.0),
-                        Point2::new(-15.0, -15.0),
-                        Point2::new(-15.0, 15.0),
-                        Point2::new(15.0, 15.0),
-                    ],
-                    2.0,
-                    graphics::WHITE,
-                )
-                .unwrap()
-                .build(ctx)
-                .unwrap(),
+            renderer: LineMeshRenderer::new(create_cube_mesh(ctx)),
             render_color: graphics::WHITE,
         }
     }
 
     pub fn render(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::draw(
-            ctx,
-            &self.mesh,
-            (
-                self.transform.position,
-                self.transform.rotation,
-                self.render_color,
-            ),
-        )
+        self.renderer.render(ctx, self);
+        self.render_color = ggez::graphics::WHITE;
+        Ok(())
     }
 }
 
-// impl Collidable for Destroyable {
-//     fn process_overlap(&mut self, _mpv: Vector2<f32>) {
-//         self.render_color = RED;
-//     }
-//     fn process_collision(&mut self, other: &impl Collidable, n: &Vector2<f32>, t: f32){
+impl Renderable for Destroyable {
+    fn position(&self) -> ggez::nalgebra::Point2<f32> {
+        ggez::nalgebra::Point2::new(self.transform.position.x, self.transform.position.y)
+    }
+    fn rotation(&self) -> f32 {
+        self.transform.rotation
+    }
+    fn color(&self) -> ggez::graphics::Color {
+        self.render_color
+    }
+}
 
 //     }
 
