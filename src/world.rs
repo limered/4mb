17332d1077bf21e85b1c4f88f406738c6362092b
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-07-09 01:37:51 +0200

    add game world to simulate game

---
+++ src/world.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::*;
+
+pub struct GameWorld {
+
+}
+
+impl GameWorld {
+  pub fn new() -> Self {
+    GameWorld{}
+  }
+
+  pub fn update(&mut self, _game_time:f32, _dt:f32) {}
+  pub fn interpolate(&mut self, _alpha:f32){
+    // let state: State = curr * alpha + prev * (1.0 - alpha);
+  }
+  pub fn render(&mut self){
+    clear_background(BLACK);
+
+    draw_poly_lines(40.0, 40.0, 6, 50.0, 0.0, 2.0, BLUE);
+  }
+}
